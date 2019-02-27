48ae39cad8966805d181d0d7014b0b3c2f7d2468
Emily Wallace - Price <emilyprice84@outlook.com>
2019-02-27 23:39:00 +0000

    first commit

---
+++ contacts/Migrations/201902271350323_CreatedContactEntity.cs
@@ -0,0 +1,16 @@
+namespace contacts.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatedContactEntity : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
